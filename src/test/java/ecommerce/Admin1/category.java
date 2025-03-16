645d07663f4bfdd4ac0771af9c070337d47a5cbb
DELL <DELL@DESKTOP-89KG4PA>
2025-03-16 20:59:50 +0530

    second commit

--- src/test/java/ecommerce/Admin1/category.java
+++ src/test/java/ecommerce/Admin1/category.java
@@ -1,8 +1,5 @@
 package ecommerce.Admin1;
 
-
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -20,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -35,51 +33,45 @@ import com.pauseandshop.ecommerce.generic.objectrepository.InsertProductPage;
 import com.pauseandshop.ecommerce.generic.objectrepository.ManageProductsPage;
 import com.pauseandshop.ecommerce.generic.objectrepository.SubCategoryPage;
 import com.pauseandshop.ecommerce.generic.webdriverutility.UtilityClassObject;
+
 @Listeners(com.pauseandshop.ecommerce.generic.listenerUtility.ListenerImp.class)
 public class category extends BaseClass {
-	@Test(groups= "regressionTest")
-	
-	public void Category() throws EncryptedDocumentException, IOException, InterruptedException {
-		
+	@Test(groups = "regressionTest")
 
-	
-	//read test script data from Excel file
+	public void Category() throws EncryptedDocumentException, IOException, InterruptedException {
+
+		// read test script data from Excel file
 		UtilityClassObject.getTest().log(Status.INFO, "read data from excel");
-	String cat=elib.getDataFromExcel("sheet1", 4, 2);
-	UtilityClassObject.getTest().log(Status.INFO, "read data from excel");
-	String catdes=elib.getDataFromExcel("sheet1", 4, 3);
-Thread.sleep(1000);
-	
-UtilityClassObject.getTest().log(Status.INFO, "clicking on category button");
-	CreateCategoryPage ccp= new CreateCategoryPage(driver);
-	Thread.sleep(1000);
-	ccp.getCategorybtn().click();
-	Thread.sleep(1000);
-	UtilityClassObject.getTest().log(Status.INFO, "sending category name to textbox");
-	ccp.getCattxt().sendKeys(cat);
-	Thread.sleep(1000);
-	UtilityClassObject.getTest().log(Status.INFO, "sending category description");
-	ccp.getCatDescriptiontxt().sendKeys(catdes);
-	UtilityClassObject.getTest().log(Status.INFO, "click on submit button");
-	ccp.getCatsubmitbtn().click();
-	Thread.sleep(1000);
-	UtilityClassObject.getTest().log(Status.INFO, "sending category name to txtbx");
-	ccp.getCatSearchbtn().sendKeys(cat);
-	Thread.sleep(1000);
-	UtilityClassObject.getTest().log(Status.INFO, "finding category name dynamically");
-	String actcat = driver.findElement(By.xpath("//td[text()='"+cat+"']")).getText();
-	Thread.sleep(1000);
-	UtilityClassObject.getTest().log(Status.PASS, "verifying the category name");
-	if(actcat.equals(cat))
-	{
-		System.out.println(cat+"information verified==PASS");
-	}else
-	{
-		System.out.println(cat+"information is not verified==FAIL");
+		String cat = elib.getDataFromExcel("sheet1", 4, 2);
+		UtilityClassObject.getTest().log(Status.INFO, "read data from excel");
+		String catdes = elib.getDataFromExcel("sheet1", 4, 3);
+		Thread.sleep(1000);
+
+		UtilityClassObject.getTest().log(Status.INFO, "clicking on category button");
+		CreateCategoryPage ccp = new CreateCategoryPage(driver);
+		Thread.sleep(1000);
+		ccp.getCategorybtn().click();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "sending category name to textbox");
+		ccp.getCattxt().sendKeys(cat);
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "sending category description");
+		ccp.getCatDescriptiontxt().sendKeys(catdes);
+		UtilityClassObject.getTest().log(Status.INFO, "click on submit button");
+		ccp.getCatsubmitbtn().click();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "sending category name to txtbx");
+		ccp.getCatSearchbtn().sendKeys(cat);
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "finding category name dynamically");
+		String actcat = driver.findElement(By.xpath("//td[text()='" + cat + "']")).getText();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.PASS, "verifying the category name");
+		Assert.assertEquals(true, actcat.contains(cat));
+
 	}
-	
-}
-	@Test(groups= "regressionTest")
+
+	@Test(groups = "regressionTest")
 	public void InsertProdDetails() throws EncryptedDocumentException, Throwable {
 
 		UtilityClassObject.getTest().log(Status.INFO, "getting data from excel");
@@ -93,151 +85,134 @@ UtilityClassObject.getTest().log(Status.INFO, "clicking on category button");
 		UtilityClassObject.getTest().log(Status.INFO, "getting data from excel");
 		String ProductDescription = elib.getDataFromExcel("sheet1", 1, 6);
 		UtilityClassObject.getTest().log(Status.INFO, "getting data from excel");
-		String ProductShippingCharge= elib.getDataFromExcel("sheet1", 1, 7);
-		
-		//insert product
-		InsertProductPage ipp=new InsertProductPage(driver);
+		String ProductShippingCharge = elib.getDataFromExcel("sheet1", 1, 7);
+
+		// insert product
+		InsertProductPage ipp = new InsertProductPage(driver);
 		UtilityClassObject.getTest().log(Status.INFO, "clicking on insertbtn");
 		ipp.getInsertbtn().click();
 		Thread.sleep(1000);
-		
-		WebElement dropdowncat=ipp.getInsertDropdwn();
+
+		WebElement dropdowncat = ipp.getInsertDropdwn();
 		UtilityClassObject.getTest().log(Status.INFO, "select dropdown option");
 		wlib.selectByValue(dropdowncat, "3");
 		Thread.sleep(1000);
-		WebElement dropdownsub=ipp.getInsertsubcatdropdown();
+		WebElement dropdownsub = ipp.getInsertsubcatdropdown();
 		Thread.sleep(1000);
 		UtilityClassObject.getTest().log(Status.INFO, "select dropdown option");
 		wlib.selectByValue(dropdowncat, "8");
 		Thread.sleep(1000);
 		UtilityClassObject.getTest().log(Status.INFO, "send productname");
-ipp.getInProdNameTxt().sendKeys(productname);
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send productcompany name");
-ipp.getInProdComptxt().sendKeys(productcompany);
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send productprice before ");
-ipp.getInProdPriceBfTxt().sendKeys(productpricebefore);
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send productprice after ");
-ipp.getInProdPriceAfTxt().sendKeys(productpriceafter);
+		ipp.getInProdNameTxt().sendKeys(productname);
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send productcompany name");
+		ipp.getInProdComptxt().sendKeys(productcompany);
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send productprice before ");
+		ipp.getInProdPriceBfTxt().sendKeys(productpricebefore);
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send productprice after ");
+		ipp.getInProdPriceAfTxt().sendKeys(productpriceafter);
 
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send Product Description ");
-ipp.getInProdDesTxt().sendKeys(ProductDescription);
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "ProductShippingCharge ");
-ipp.getInProdShipChargesTxt().sendKeys(ProductShippingCharge);
-Thread.sleep(1000);
-WebElement productavailable =ipp.getInProdAvailableDropdwn();
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "productavailable ");
-wlib.selectByValue(productavailable, "In Stock");
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send InProdImage1 ");
-WebElement choosefile1 = ipp.getInProdImage1();
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send image address ");
-File fileupload=new File("C:\\Users\\DELL\\eclipse-workspace\\SeleniumECommerceFramework\\image\\harry.png");
-choosefile1.sendKeys(fileupload.getAbsolutePath());
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send InProdImage2 ");
-WebElement choosefile2 =ipp.getInProdImage2();
-UtilityClassObject.getTest().log(Status.INFO, "send image address ");
-File fileupload2=new File("C:\\Users\\DELL\\eclipse-workspace\\SeleniumECommerceFramework\\image\\harryyy.jpg");
-choosefile2.sendKeys(fileupload.getAbsolutePath());
-Thread.sleep(1000);
-UtilityClassObject.getTest().log(Status.INFO, "send InProdImage3 ");
-WebElement choosefile3 =ipp.getInProdImage3();
-UtilityClassObject.getTest().log(Status.INFO, "send image address ");
-File fileupload3=new File("C:\\Users\\DELL\\eclipse-workspace\\SeleniumECommerceFramework\\image\\potter.jpg");
-choosefile3.sendKeys(fileupload.getAbsolutePath());
-UtilityClassObject.getTest().log(Status.INFO, "click on submit btn ");
-ipp.getInSubmitBtn().sendKeys(Keys.ENTER);
-//driver.findElement(By.xpath("//button[@class='btn']")).sendKeys(Keys.ENTER);
-ManageProductsPage mpp= new ManageProductsPage(driver);
-UtilityClassObject.getTest().log(Status.INFO, "click on manage btn");
-mpp.getManageprodbtn().click();
-UtilityClassObject.getTest().log(Status.INFO, "search btn ");
-  WebElement search = driver.findElement(By.xpath("//input[@type='text']"));
-  UtilityClassObject.getTest().log(Status.INFO, "send Product naem");
-  search.sendKeys(productname);
-  UtilityClassObject.getTest().log(Status.INFO, "search dynamically productname");
-  String actprod =driver.findElement(By.xpath("//td[text()='"+productname+"']")).getText();
-  Thread.sleep(1000);
-  UtilityClassObject.getTest().log(Status.INFO, "verify ");
- if(actprod.trim().contains(productname)) {
-  System.out.println(productname+"information verified==PASS"); }else {
-  System.out.println(productname+"information is not verified==FAIL"); }
- 
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send Product Description ");
+		ipp.getInProdDesTxt().sendKeys(ProductDescription);
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "ProductShippingCharge ");
+		ipp.getInProdShipChargesTxt().sendKeys(ProductShippingCharge);
+		Thread.sleep(1000);
+		WebElement productavailable = ipp.getInProdAvailableDropdwn();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "productavailable ");
+		wlib.selectByValue(productavailable, "In Stock");
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send InProdImage1 ");
+		WebElement choosefile1 = ipp.getInProdImage1();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send image address ");
+		File fileupload = new File("C:\\Users\\DELL\\eclipse-workspace\\SeleniumECommerceFramework\\image\\harry.png");
+		choosefile1.sendKeys(fileupload.getAbsolutePath());
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send InProdImage2 ");
+		WebElement choosefile2 = ipp.getInProdImage2();
+		UtilityClassObject.getTest().log(Status.INFO, "send image address ");
+		File fileupload2 = new File(
+				"C:\\Users\\DELL\\eclipse-workspace\\SeleniumECommerceFramework\\image\\harryyy.jpg");
+		choosefile2.sendKeys(fileupload.getAbsolutePath());
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send InProdImage3 ");
+		WebElement choosefile3 = ipp.getInProdImage3();
+		UtilityClassObject.getTest().log(Status.INFO, "send image address ");
+		File fileupload3 = new File(
+				"C:\\Users\\DELL\\eclipse-workspace\\SeleniumECommerceFramework\\image\\potter.jpg");
+		choosefile3.sendKeys(fileupload.getAbsolutePath());
+		UtilityClassObject.getTest().log(Status.INFO, "click on submit btn ");
+		ipp.getInSubmitBtn().sendKeys(Keys.ENTER);
+
+		ManageProductsPage mpp = new ManageProductsPage(driver);
+		UtilityClassObject.getTest().log(Status.INFO, "click on manage btn");
+		mpp.getManageprodbtn().click();
+		UtilityClassObject.getTest().log(Status.INFO, "search btn ");
+		WebElement search = driver.findElement(By.xpath("//input[@type='text']"));
+		UtilityClassObject.getTest().log(Status.INFO, "send Product naem");
+		search.sendKeys(productname);
+		UtilityClassObject.getTest().log(Status.INFO, "search dynamically productname");
+		String actprod = driver.findElement(By.xpath("//td[text()='" + productname + "']")).getText();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "verify ");
+		Assert.assertEquals(true, actprod.contains(productname));
+
 	}
-	@Test(groups= "regressionTest")
+
+	@Test(groups = "regressionTest")
 	public void Subcategory() throws EncryptedDocumentException, IOException, InterruptedException {
-		
-String subcat=elib.getDataFromExcel("sheet1", 4, 1);
-UtilityClassObject.getTest().log(Status.INFO, "waiting to page to load");
+
+		String subcat = elib.getDataFromExcel("sheet1", 4, 1);
+		UtilityClassObject.getTest().log(Status.INFO, "waiting to page to load");
 		wlib.waitForPageToLoad(driver);
-		SubCategoryPage scp=new SubCategoryPage(driver);
+		SubCategoryPage scp = new SubCategoryPage(driver);
 		Thread.sleep(1000);
 		UtilityClassObject.getTest().log(Status.INFO, "click on subcatbtn ");
 		scp.getSubcatbtn().click();
 		Thread.sleep(1000);
-		WebElement dropdownsub=scp.getSubCatdropdown();
+		WebElement dropdownsub = scp.getSubCatdropdown();
 		UtilityClassObject.getTest().log(Status.INFO, "select the dropdown option ");
 		wlib.selectByValue(dropdownsub, "3");
-	Thread.sleep(1000);
-	UtilityClassObject.getTest().log(Status.INFO, "send subcat name ");
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send subcat name ");
 		scp.getSubcatnametxt().sendKeys(subcat);
 		Thread.sleep(1000);
 		UtilityClassObject.getTest().log(Status.INFO, "click on save btn");
-      scp.getSavebtn().click();
-      Thread.sleep(1000);
-      UtilityClassObject.getTest().log(Status.INFO, "send subcat in searchbtn ");
+		scp.getSavebtn().click();
+		Thread.sleep(1000);
+		UtilityClassObject.getTest().log(Status.INFO, "send subcat in searchbtn ");
 		scp.getSearchbtn().sendKeys(subcat);
 		Thread.sleep(1000);
 		UtilityClassObject.getTest().log(Status.INFO, "dynamically search subcat name ");
-		String actsub = driver.findElement(By.xpath("//td[text()='"+subcat+"']")).getText();
+		String actsub = driver.findElement(By.xpath("//td[text()='" + subcat + "']")).getText();
 		Thread.sleep(1000);
 		UtilityClassObject.getTest().log(Status.PASS, "verify subcat name");
-		if(actsub.equals(subcat))
-		{
-			System.out.println(subcat +" information verified==PASS");
-		}else
-		{
-			System.out.println(subcat + "information is not verified==FAIL");
-		}
-		
-		
+		Assert.assertEquals(true, actsub.contains(subcat));
+
 	}
-	@Test(groups= "smokeTest")
-public void InsertProductSelectOneDropdown() throws EncryptedDocumentException, IOException {
+
+	@Test(groups = "smokeTest")
+	public void InsertProductSelectOneDropdown() throws EncryptedDocumentException, IOException {
 		UtilityClassObject.getTest().log(Status.INFO, "getting data from excel");
-	String InsertProd=elib.getDataFromExcel("sheet1", 1, 0);
-	
-	//insert product
-	InsertProductPage ipp=new InsertProductPage(driver);
-	UtilityClassObject.getTest().log(Status.INFO, "click on insertbtn");
-	ipp.getInsertbtn().click();
-	UtilityClassObject.getTest().log(Status.INFO, "click on dropdonw");
-	WebElement dropdown = ipp.getInsertDropdwn();
-	UtilityClassObject.getTest().log(Status.INFO, "select dropdown option");
-	wlib.selectByValue(dropdown,"3");
-	UtilityClassObject.getTest().log(Status.INFO, "get insertproduct name");
-	String actInsertProd = ipp.getVerifyinserttxt().getText();
-	UtilityClassObject.getTest().log(Status.PASS, "verifying insertproduct name");
-	if(actInsertProd.equals(InsertProd))
-	{
-		System.out.println(InsertProd+"information verified==PASS");
-	}else
-	{
-		System.out.println(InsertProd+"information is not verified==FAIL");
+		String InsertProd = elib.getDataFromExcel("sheet1", 1, 0);
+
+		// insert product
+		InsertProductPage ipp = new InsertProductPage(driver);
+		UtilityClassObject.getTest().log(Status.INFO, "click on insertbtn");
+		ipp.getInsertbtn().click();
+		UtilityClassObject.getTest().log(Status.INFO, "click on dropdonw");
+		WebElement dropdown = ipp.getInsertDropdwn();
+		UtilityClassObject.getTest().log(Status.INFO, "select dropdown option");
+		wlib.selectByValue(dropdown, "3");
+		UtilityClassObject.getTest().log(Status.INFO, "get insertproduct name");
+		String actInsertProd = ipp.getVerifyinserttxt().getText();
+		UtilityClassObject.getTest().log(Status.PASS, "verifying insertproduct name");
+		Assert.assertEquals(true, actInsertProd.contains(InsertProd));
+
 	}
-	
 }
-}
-
-
-
-
-
-	
